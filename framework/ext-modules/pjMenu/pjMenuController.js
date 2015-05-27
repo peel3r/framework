d4bb2b5af03b3b0956338e11605206fe91d9adc2
peel3r <p@weljakubowski.com>
2015-05-27 20:30:23 +0100

    started menu feature

---
+++ framework/ext-modules/pjMenu/pjMenuController.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+
+angular.module('pjMenu').controller('pjMenuController',
+    [$scope,
+        function ($scope) {
+
+        }
+
+    ])
---
+++ framework/ext-modules/pjMenu/pjMenuDirective.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+angular.module('pjMenu').directive("pjMenu", function () {
+    return {
+        transclude: true,
+        templateUrl: 'ext-modules/pjMenu/pjMenuTemplate.html',
+        controller: 'pjMenuController',
+        link: function (scope, el, attr) {
+
+        }
+
+    };
+});
